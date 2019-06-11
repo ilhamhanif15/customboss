bcaf4b6114069d4c179294afe753a2f2b77e2f26
faisalfrtz <anandafaisalf20>
2019-06-11 22:54:58 +0700

    cara pemesanan update

--- application/views/admin/layout/navbar.php
+++ application/views/admin/layout/navbar.php
@@ -63,7 +63,7 @@
           Activity Log
         </a>
         <div class="dropdown-divider"></div>
-        <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">
+        <a class="dropdown-item" href="<?= base_url().'admin/login'?>" data-toggle="modal" data-target="#logoutModal">
           <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
           Logout
         </a>
